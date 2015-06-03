fea8c89e5d7f6687733484aaf3e4153522f14af5
karaluo09 <karaluo09@gmail.com>
2015-06-03 10:30:04 -0400

    test phone sensor

--- 2016-ProjRobo/sdk-worldsNew/ftc_sdk/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/IntegratedSDK/testSensor.java
+++ 2016-ProjRobo/sdk-worldsNew/ftc_sdk/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/IntegratedSDK/testSensor.java
@@ -3,5 +3,8 @@ package com.qualcomm.ftcrobotcontroller.IntegratedSDK;
 /**
  * Created by Lenovo on 6/3/2015.
  */
+import android.hardware.SensorManager;
 public class testSensor {
+    SensorManager sMgr;
+    //sMgr = (SensorManager)this.
 }

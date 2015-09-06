df24b99d95c0461e36c0ca55ef6a3a7e07b4bb7f
eula1998 <eula1998@gmail.com>
2015-09-06 03:05:17 -0400

    test test for rookie reference

---
+++ Beta test 6.4.2015/ftc_app-master/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Op8379.java
@@ -0,0 +1,25 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+/**
+ * Created by zht on 8/31/2015.
+ */
+public class Op8379 extends OpMode {
+
+    @Override
+    public void start()
+    {
+
+    }
+
+    @Override
+    public void loop()
+    {
+
+    }
+
+    @Override
+    public void stop()
+    {
+
+    }
+}

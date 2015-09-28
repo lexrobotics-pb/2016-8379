ebe28c6354a24e479dd33985a4298b7c3274eedf
eula1998 <eula1998@gmail.com>
2015-09-28 16:20:11 -0400

    fixing AS problem + basis chasis testing

---
+++ ftc_app-master 9.24.2015/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ChassisTest.java
@@ -0,0 +1,63 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.robocol.Telemetry;
+
+/**
+ * Created by zht on 9/24/2015.
+ */
+public class ChassisTest extends OpMode {
+
+    final static double DEADZONE= 0.1;
+    DcMotor motorRight;
+    DcMotor motorLeft;
+
+    Telemetry telemetry = new Telemetry();
+
+
+    @Override
+    public void init()
+    {
+        motorRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorRight.setDirection(DcMotor.Direction.REVERSE); //reverses back right motor
+        motorLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+    }
+
+    @Override
+    public void loop()
+    {
+
+        /*Sets joystick deadzone. The joysticks we used before went to +/- 100,
+        but these tutorials all use +/- 1...so the deadzone should be .1 instead of 10, I guess?.
+        Also, checking the documentation, there is a class called
+        public void setJoystickDeadZone(float deadzone),
+        but I couldn't figure out how to implement it*/
+
+        if (Math.abs(gamepad1.left_stick_x) < DEADZONE){
+            gamepad1.left_stick_x = 0;
+        }
+        if (Math.abs(gamepad1.left_stick_y) < DEADZONE){
+            gamepad1.left_stick_y = 0;
+        }
+        if (Math.abs(gamepad1.right_stick_x) < DEADZONE){
+            gamepad1.right_stick_x = 0;
+        }
+        if (Math.abs(gamepad1.right_stick_y) < DEADZONE){
+            gamepad1.right_stick_y = 0;
+        }
+
+              /*---------------------Movement----------------------------*/
+        motorLeft.setPower(gamepad1.left_stick_x);
+        motorRight.setPower(gamepad1.right_stick_x);
+        telemetry.addData("*************************", "Working");
+    }
+
+    @Override
+    public void stop()
+    {
+
+    }
+
+}

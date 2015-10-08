b13791226386957849c4403bee18e0c517101991
karaluo09 <karaluo09@gmail.com>
2015-10-08 14:54:53 -0400

    Color sensor with LED control

---
+++ ftc_app-master 9.24.2015/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/ColorTest.java
@@ -0,0 +1,47 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import android.util.Log;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.robocol.Telemetry;
+
+/**
+ * Created by Kara Luo on 10/7/2015.
+ */
+public class ColorTest extends OpMode {
+    ColorSensor color;
+    float hsvValues[] = {0,0,0};
+    final float values[] = hsvValues;
+
+    @Override
+    public void init()
+    {
+        color = hardwareMap.colorSensor.get("color");
+    }
+    @Override
+    public void loop()
+    {
+        color.enableLed(false);
+        telemetry.addData("Clear", color.alpha());
+        telemetry.addData("Red  ", color.red());
+        telemetry.addData("Green", color.green());
+        telemetry.addData("Blue ", color.blue());
+        telemetry.addData("Hue", hsvValues[0]);
+    }
+
+    @Override
+    public void stop()
+    {
+
+    }
+
+
+}
+
+

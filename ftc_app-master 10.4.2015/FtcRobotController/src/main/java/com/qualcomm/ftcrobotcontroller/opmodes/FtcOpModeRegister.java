5f183356c632a28972f8ed76613c4c5746bfdc31
karaluo09 <karaluo09@gmail.com>
2015-10-21 19:38:47 -0400

    color sensor calibration

--- ftc_app-master 10.4.2015/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ ftc_app-master 10.4.2015/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -59,6 +59,7 @@ public class FtcOpModeRegister implements OpModeRegister {
 
     manager.register("MatrixK9TeleOp", MatrixK9TeleOp.class);
     manager.register("K9TeleOp", K9TeleOp.class);
+    manager.register("LinearColor", LinearColorTest.class);
     manager.register ("PushBotAuto", PushBotAuto.class);
     manager.register ("PushBotManual", PushBotManual.class);
       manager.register ("ChassisTest", ChassisTest.class);

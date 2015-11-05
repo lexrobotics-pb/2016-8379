aca4ce4231b56ecf8df9270c4dc01ee7109366d2
karaluo09 <karaluo09@gmail.com>
2015-11-04 19:19:14 -0500

    clean up

--- Backup/Action.java
+++
@@ -1,7 +0,0 @@
-package com.qualcomm.ftcrobotcontroller.IntegratedSDK;
-
-public abstract class Action {
-    abstract boolean isFinished(RobotStateFix state); //requires READ_ONLY, returns true if action is done
-    abstract boolean update(RobotStateFix state); //requires READ_ONLY, reads from devices, updates robotState, returns true if an update is required
-    abstract void doAction(RobotStateFix state); //requires WRITE_ONLY
-}

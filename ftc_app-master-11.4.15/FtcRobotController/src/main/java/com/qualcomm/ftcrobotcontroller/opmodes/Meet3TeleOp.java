8ded97c4256cd5b0157abf50878e5c0646f2be02
eula1998 <eula1998@gmail.com>
2016-01-11 18:39:44 -0500

    TeleOp drafted for Meet 3

---
+++ ftc_app-master-11.4.15/FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Meet3TeleOp.java
@@ -0,0 +1,144 @@
+package com.qualcomm.ftcrobotcontroller.opmodes;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
+
+/**
+ * Created by Eula on 1/11/16.
+ * Edited by Eula on 1/11/16, drafted for Meet 3
+ */
+public class Meet3TeleOp extends OpMode {
+
+    final static double DEADZONE = 0.1;
+    DcMotor motorFrontRight;
+    DcMotor motorFrontLeft;
+    DcMotor motorBackRight;
+    DcMotor motorBackLeft;
+    DcMotor Flipper;
+    DcMotor Box;
+
+
+    Servo LeftTrigger;
+    Servo RightTrigger;
+    Servo dump;
+    Servo push;
+    Servo conveyor;
+
+    double conveyorPower;
+    double current;
+    double boxPower;
+    double dumpPosition;
+    double pushPosition;
+    double speedFactor = 0.9;
+
+    boolean speed = true;
+
+
+    @Override
+    public void init() {
+        motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+        motorBackRight.setDirection(DcMotor.Direction.REVERSE); //forwards back left motor
+        motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        motorFrontRight.setDirection(DcMotor.Direction.REVERSE); //forwards back left motor
+        motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        motorBackLeft.setDirection(DcMotor.Direction.FORWARD); //forwards back left motor
+        motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        motorFrontLeft.setDirection(DcMotor.Direction.FORWARD); //forwards front left motor
+        Flipper = hardwareMap.dcMotor.get("Flipper");
+        Box = hardwareMap.dcMotor.get("Box");
+
+        push = hardwareMap.servo.get("push");
+        dump = hardwareMap.servo.get("dump");
+        LeftTrigger = hardwareMap.servo.get("LeftTrigger");
+        RightTrigger = hardwareMap.servo.get("RightTrigger");
+        conveyor = hardwareMap.servo.get("conveyor");
+
+        RightTrigger.setPosition(0.95);
+        LeftTrigger.setPosition(0.15);
+        dump.setPosition(0.5);
+        push.setPosition(0.5);
+        conveyor.setPosition(0.5);
+    }
+
+    @Override
+    public void loop() {
+//===================Movement==================================================
+        if (Math.abs(gamepad1.left_stick_x) < DEADZONE) {gamepad1.left_stick_x = 0;}
+        if (Math.abs(gamepad1.left_stick_y) < DEADZONE) {gamepad1.left_stick_y = 0;}
+        if (Math.abs(gamepad1.right_stick_x) < DEADZONE) {gamepad1.right_stick_x = 0;}
+        if (Math.abs(gamepad1.right_stick_y) < DEADZONE) {gamepad1.right_stick_y = 0;}
+
+        motorFrontLeft.setPower(gamepad1.left_stick_y * speedFactor);
+        motorBackLeft.setPower(gamepad1.left_stick_y * speedFactor);
+        motorFrontRight.setPower(gamepad1.right_stick_y * speedFactor);
+        motorBackRight.setPower(gamepad1.right_stick_y * speedFactor);
+
+        if (gamepad1.y) speedFactor*=-1;
+
+        if (gamepad1.right_bumper){
+            if (speed)
+            {   speed = false;
+                speedFactor = speedFactor/0.9*0.75;}
+            else
+            {   speed = true;
+                speedFactor = speedFactor/0.75*0.9;}
+        }
+
+//==========Flipper=====================
+        if(gamepad1.b)  Flipper.setPower(0.95);
+        if(gamepad1.a)  Flipper.setPower(0.0);
+        if(gamepad1.x)  Flipper.setPower(-0.8);
+
+//============push====================
+        pushPosition = 0.5;
+        if (gamepad1.left_bumper)    pushPosition = 0.3;
+        if (gamepad1.left_trigger>0.3)  pushPosition = 0.85;
+        push.setPosition(pushPosition);
+
+
+//************************ Gamepad 2 ***********************************
+//========conveyor=============================
+        conveyorPower = 0.5;
+        current = gamepad2.left_stick_x;
+        if (Math.abs(current) > 0.5)//right = going to the right when on the ramp
+            conveyorPower = 0.5 + Math.abs(current)/current*0.45;
+        conveyor.setPosition(conveyorPower);
+//==========Box==============================
+        boxPower = 0.0;
+        if (gamepad2.a) boxPower = -0.3;
+        if (gamepad2.y) boxPower = 0.3;
+
+        Box.setPower(boxPower);
+//===========dump=====================
+        dumpPosition = 0.5;
+        if (gamepad2.x) dumpPosition = 0.3;
+        if (gamepad2.b) dumpPosition = 0.85;
+
+        dump.setPosition(dumpPosition);
+
+//===========Triggers============================
+        if(gamepad2.left_bumper)
+            LeftTrigger.setPosition(0.15);
+        else if(gamepad2.left_trigger > 0.3)
+            LeftTrigger.setPosition(0.9);
+
+        if(gamepad2.right_bumper)
+            RightTrigger.setPosition(0.95);
+        else if(gamepad2.right_trigger > 0.3)
+            RightTrigger.setPosition(0.2);
+
+    }
+
+    @Override
+    public void stop() {
+        motorFrontLeft.setPower(0.0);
+        motorBackLeft.setPower(0.0);
+        motorFrontRight.setPower(0.0);
+        motorBackRight.setPower(0.0);
+        Flipper.setPower(0.0);
+        Box.setPower(0.0);
+        dump.setPosition(0.5);
+        push.setPosition(0.5);
+        conveyor.setPosition(0.5);
+    }
+}
